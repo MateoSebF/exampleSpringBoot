d31b1fd12ba268ff2469f4ec0b29b9a033fdd582
unknown <mateo.forero-f@labinfo.is.escuelaing.edu.co>
2025-02-15 12:15:16 -0500

    feat: change

--- src/main/java/com/base/game/MovementController.java
+++ src/main/java/com/base/game/MovementController.java
@@ -1,21 +1,31 @@
 package com.base.game;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.WebSocketSession;
 
 @Controller
 public class MovementController {
 
-    // Posición inicial del jugador (para simplificar, se usa una variable global)
-    private Position position = new Position(0, 0);
+    private final ConcurrentHashMap<String, Position> positions = new ConcurrentHashMap<>();
+
     private static final int SPEED = 5; // Incremento en cada tick
 
     // Mapea los mensajes enviados a /app/move
     @MessageMapping("/move")
-    // Envía la posición actualizada a /topic/position
-    @SendTo("/topic/position")
-    public Position processMovement(MovementState movement) throws Exception {
+    public void processMovement(MovementState movement, WebSocketSession session) throws Exception {
+        Position position;
+        if (positions.get(session.getId())== null){
+            position = new Position(0,0);
+            positions.put(session.getId(), position);
+        }
+        else{
+            position = positions.get(session.getId());
+        }
         // Actualiza la posición en función de las teclas presionadas
         if (movement.isUp()) {
             position.setY(position.getY() - SPEED);
@@ -30,8 +40,12 @@ public class MovementController {
             position.setX(position.getX() + SPEED);
         }
         System.out.println(movement);
-        // Se podría agregar un pequeño retardo simulado si es necesario:
-        Thread.sleep(10);
-        return position;
+    }
+
+    @Scheduled(fixedRate = 50) // Ejecuta cada 1 segundos
+    @SendTo("/topic/positions")  // Envía las posiciones a todos los suscriptores
+    public ConcurrentHashMap<String, Position> enviarPosiciones() {
+        // Enviar todas las posiciones de los cuadrados
+        return positions;
     }
 }
